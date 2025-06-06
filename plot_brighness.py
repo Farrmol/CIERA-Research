4df3f4660be1d9a1ea87f623a091823cb1099551
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-06-06 11:54:21 -0700

    outline of code to plot brightness

---
+++ plot_brighness.py
@@ -0,0 +1,15 @@
+
+
+# TODO: load the brightness data
+
+# TODO: make a plot of brightness data vs time (analagous to red stars in orbit plot)
+
+# TODO: load the posterior values
+
+# TODO: for each set of orbital parameters (e.g. semimajor axis, eccentricity, etc)
+# calculate the expected brightness at each data point's time  
+# (using orbitize's code to compute brightness)
+
+# TODO: for each set of predicted brightnesses, add to the plot (analagous to the grey lines in the orbit plot)
+
+# flow of logic: [sma, ecc, inc, ...] -> orbit -> brightness vs time -> [brightness1(time 1), brightness2(time 2), ...]
